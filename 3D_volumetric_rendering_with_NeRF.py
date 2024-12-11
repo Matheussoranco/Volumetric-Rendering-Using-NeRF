88f32c503e643d0aa5c4e94c869f1cfadfa89be2
Matheus Pullig Soranço de Carvalho <matheussoranco@gmail.com>
2024-12-11 11:08:25 -0300

    setting up and loading data

---
+++ 3D_volumetric_rendering_with_NeRF.py
@@ -0,0 +1,36 @@
+import os
+
+os.environ["KERAS_BACKEND"] = "tensorflow"
+
+import tensorflow as tf
+
+tf.random.set_seed(42)
+
+import keras
+from keras import layers
+import os
+import glob
+import imageio.v2 as imageio
+import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+AUTO = tf.data.AUTOTUNE
+BATCH_SIZE = 5
+NUM_SAMPLES = 32
+POS_ENCODE_DIMS = 16
+EPOCHS = 20
+
+url = (
+    "http://cseweb.ucsd.edu/~viscomp/projects/LF/papers/ECCV20/nerf/tiny_nerf_data.npz"
+)
+data = keras.utils.get_file(origin=url)
+
+data = np.load(data)
+images = data["images"]
+im_shape = images.shape
+(num_images, H, W, _) = images.shape
+(poses, focal) = (data["poses"], data["focal"])
+
+plt.imshow(images[np.random.randit(low=0, high=num_images)])
+plt.show()
