335047ec849169033a0058ee649e206848000f93
Matheus Pullig Soranço de Carvalho <matheussoranco@gmail.com>
2024-12-15 16:33:55 -0300

    Implemented translation and rotation

--- 3D_volumetric_rendering_with_NeRF.py
+++ 3D_volumetric_rendering_with_NeRF.py
@@ -363,3 +363,82 @@ for ax, ori_img, recons_img, depth_map in zip(
 
     ax[2].imshow(keras.utils.array_to_img(depth_map[..., None]), cmap="inferno")
     ax[2].set_title("Depth Map")
+
+def get_translation_t(t):
+    """Obtenha a matriz de tradução para movimento em t."""
+    matrix = [
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, t],
+        [0, 0, 0, 1],
+    ]
+    return tf.convert_to_tensor(matrix, dtype=tf.float32)
+
+
+def get_rotation_phi(phi):
+    """Obtenha a matriz de rotação para movimento em phi."""
+    matrix = [
+        [1, 0, 0, 0],
+        [0, tf.cos(phi), -tf.sin(phi), 0],
+        [0, tf.sin(phi), tf.cos(phi), 0],
+        [0, 0, 0, 1],
+    ]
+    return tf.convert_to_tensor(matrix, dtype=tf.float32)
+
+
+def get_rotation_theta(theta):
+    """Obtenha a matriz de rotação para movimento em teta."""
+    matrix = [
+        [tf.cos(theta), 0, -tf.sin(theta), 0],
+        [0, 1, 0, 0],
+        [tf.sin(theta), 0, tf.cos(theta), 0],
+        [0, 0, 0, 1],
+    ]
+    return tf.convert_to_tensor(matrix, dtype=tf.float32)
+
+
+def pose_spherical(theta, phi, t):
+    """
+    Leve a câmera para a matriz mundial para o theta, phi correspondente
+e t.
+    """
+    c2w = get_translation_t(t)
+    c2w = get_rotation_phi(phi / 180.0 * np.pi) @ c2w
+    c2w = get_rotation_theta(theta / 180.0 * np.pi) @ c2w
+    c2w = np.array([[-1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]]) @ c2w
+    return c2w
+
+
+rgb_frames = []
+batch_flat = []
+batch_t = []
+
+for index, theta in tqdm(enumerate(np.linspace(0.0, 360.0, 120, endpoint=False))):
+    c2w = pose_spherical(theta, -30.0, 4.0)
+
+    #
+    ray_oris, ray_dirs = get_rays(H, W, focal, c2w)
+    rays_flat, t_vals = render_flat_rays(
+        ray_oris, ray_dirs, near=2.0, far=6.0, num_samples=NUM_SAMPLES, rand=False
+    )
+
+    if index % BATCH_SIZE == 0 and index > 0:
+        batched_flat = tf.stack(batch_flat, axis=0)
+        batch_flat = [rays_flat]
+
+        batched_t = tf.stack(batch_t, axis=0)
+        batch_t = [t_vals]
+
+        rgb, _ = render_rgb_depth(
+            nerf_model, batched_flat, batched_t, rand=False, train=False
+        )
+
+        temp_rgb = [np.clip(255 * img, 0.0, 255.0).astype(np.uint8) for img in rgb]
+
+        rgb_frames = rgb_frames + temp_rgb
+    else:
+        batch_flat.append(rays_flat)
+        batch_t.append(t_vals)
+
+rgb_video = "rgb_video.mp4"
+imageio.mimwrite(rgb_video, rgb_frames, fps=30, quality=7, macro_block_size=None)
